d00dc54fdfc9a6cc383a47db73832ec693f52e22
Ahmed Ollaek <ahmedollaek96@gmail.com>
2020-08-08 01:30:17 +0200

    Updates

---
+++ precache-manifest.a6b798b44b03a7762d1cd243d6df406a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba64bd3ab19c63bcb99297410bdf25f",
+    "url": "/Coloring-Demo/index.html"
+  },
+  {
+    "revision": "3161e5a54d92c2c2ae19",
+    "url": "/Coloring-Demo/static/css/main.def436f8.chunk.css"
+  },
+  {
+    "revision": "fd9e7b215a97d142f309",
+    "url": "/Coloring-Demo/static/js/2.a2f8882a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Coloring-Demo/static/js/2.a2f8882a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3161e5a54d92c2c2ae19",
+    "url": "/Coloring-Demo/static/js/main.42337de9.chunk.js"
+  },
+  {
+    "revision": "16baddec557f3da2342c",
+    "url": "/Coloring-Demo/static/js/runtime-main.8bc44f11.js"
+  }
+]);
