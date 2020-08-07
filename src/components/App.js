34e6615a53d381a9659c404f693c5926834bf7fe
Ahmed Ollaek <ahmedollaek96@gmail.com>
2020-08-08 01:25:25 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Stepper from "./Stepper/Stepper";
+
+import "./App.css";
+
+function App() {
+  
+ 
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Stepper />
+      </header>
+    </div>
+  );
+}
+
+export default App;
